a35b62b69c49d3ef8b1f6e625b3e3c6574b7798f
minikaevavaleriia <valeriia@minikaeva.ru>
2023-02-14 05:32:47 +0300

    trash

--- app.py
+++ app.py
@@ -14,20 +14,31 @@ def get_db_connection():
 
 
 @app.route('/')
-def base():
-    return render_template('base.html')
-
-
-@app.route('/songs/')
-def index():
+@app.route('/home')
+def home():
+    links = "home/sirius/Загрузки/sounds_web/kakmommy.mp3"
     conn = get_db_connection()
     cur = conn.cursor()
-    cur.execute('select * from songs;')
+    cur.execute('select * from songs where name is not null;')
     songs = cur.fetchall()
     cur.close()
     conn.close()
-    return render_template('index.html', songs=songs)
+    return render_template('home.html', songs=songs, links=links)
 
-# @app.route('/base')
+
+@app.route('/music')
+def index():
+    conn = get_db_connection()
+    cur = conn.cursor()
+    cur.execute('select * from songs where album_id = 2;')
+    songs = cur.fetchall()
+    cur.close()
+    cur1 = conn.cursor()
+    cur1.execute('select name from albums where id = 2;')
+    album = cur1.fetchall()
+    cur1.close()
+    conn.close()
+    return render_template('index.html', songs=songs, album=album, x=1)
+
 
 
